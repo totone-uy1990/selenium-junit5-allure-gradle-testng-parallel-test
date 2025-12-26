6647685f98c5ae1b75fa8fc589a42eb1b6e3f8f7
totone-uy1990 <ronalggm@outlook.com>
2025-12-25 21:19:54 -0300

    refactored junit-platform.properties and TestRunner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,8 +6,11 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+//@SelectClasspathResource("features")
 
+@ConfigurationParameter(
+        key = FEATURES_PROPERTY_NAME,
+        value = "src/test/resources/features")
 @ConfigurationParameter(
         key = GLUE_PROPERTY_NAME,
         value = "steps")
@@ -15,6 +18,5 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameter(
         key = PLUGIN_PROPERTY_NAME,
         value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
-
 public class TestRunner {
 }
